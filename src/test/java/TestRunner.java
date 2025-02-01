f6271367c95cffb9a4ab4a7fc155d1bec83bb91f
minghui3 <149645776+minghui3@users.noreply.github.com>
2025-02-02 01:54:44 +0800

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,4 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(

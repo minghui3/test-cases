013f38e1455867490e04d85c4ade7170578fe9a7
minghui3 <149645776+minghui3@users.noreply.github.com>
2025-02-02 19:38:52 +0800

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,13 +1,6 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Cucumber;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    features = {"src/test/resources/features"}, 
-    glue = {"stepdefinitions"}, 
-    plugin = {"progress"}
-)
+@Cucumber
 public class TestRunner {
 
 }

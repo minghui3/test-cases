6d11e783eec3617453a3d974ee7f3fb3958d44cd
minghui3 <149645776+minghui3@users.noreply.github.com>
2025-02-02 14:15:12 +0800

    Update PushTestResult.java

--- src/test/java/utils/PushTestResult.java
+++ src/test/java/utils/PushTestResult.java
@@ -49,7 +49,7 @@ public class PushTestResult {
                 // build and send http request
                 HttpClient client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://f80f-2406-3003-2006-4502-3c43-82e6-903a-9004.ngrok-free.app/api/add-test-results"))
+                        .uri(URI.create("https://89cd-118-200-211-214.ngrok-free.app/api/add-test-results"))
                         .header("Content-Type", "application/json")
                         .POST(HttpRequest.BodyPublishers.ofString(jsonString))
                         .build();
